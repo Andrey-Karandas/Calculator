216b9790185e04d20f4ebfee90a9c8d51363ac8b
Andriy <jasperheroqt17@gmail.com>
2024-05-18 11:21:37 +0300

    modified

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const buttons = document.querySelectorAll('.button')
   const maxLength = +input.getAttribute('maxlength')
 
-  function getResult() {
-    let numbers = input.value.split(/[-+*/]/)
-    let operators = []
+  function getOperators() {
+    const operators = []
     for (let i = 0; i < input.value.length; i++) {
       if (
         input.value[i] === '+' ||
@@ -15,7 +14,12 @@ document.addEventListener('DOMContentLoaded', () => {
       )
         operators.push(input.value[i])
     }
+    return operators
+  }
 
+  function getResult() {
+    let numbers = input.value.split(/[-+*/]/)
+    const operators = getOperators()
     let result = parseInt(numbers[0])
     for (let i = 1; i < numbers.length; i++) {
       if (operators[i - 1] === '+') {
@@ -33,13 +37,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const clearInput = () => (input.value = '')
 
+  const setClass = (name) =>
+    buttons.forEach((button) => (button.className = name))
+
   document.addEventListener('click', (event) => {
     if (event.target.closest('.button')) input.value += event.target.textContent
-    if (input.value.length === maxLength) {
-      buttons.forEach((button) => (button.className = 'stop'))
-    } else {
-      buttons.forEach((button) => (button.className = 'button'))
-    }
+    input.value.length === maxLength ? setClass('stop') : setClass('button')
   })
 
   document.querySelector('.clear').addEventListener('click', clearInput)
